803829af3ff61f34394bf77d0917a81b5f43b4e0
ksukhija <kamal.sukhija@gmail.com>
2018-10-05 23:52:24 -0400

    Amost WOrking

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,8 +13,22 @@ var g_crystal2Points;
 var g_crystal3Points;
 var g_crystal4Points;
 
+
+// Variables to hold Crystal Points Status ( hidden or displayed )
+var g_crystal1PointsHidden;
+var g_crystal2PointsHidden;
+var g_crystal3PointsHidden;
+var g_crystal4PointsHidden;
+
+
 //Players Current Score
 var g_current_score;
+
+var g_game_status;
+
+// Game Win/Loss Counts
+var g_games_won = 0;
+var g_games_lost = 0;
 
 /**
  *  C O N S T A N T S
@@ -27,14 +41,39 @@ const CRYSTAL_POINTS_MIN = 1;
 const CRYSTAL_POINTS_MAX = 12;
 
 
+// Game Status Values
+const GAME_OVER_PLAYER_WON = 1;
+const GAME_OVER_PLAYER_LOST = 2;
+const GAME_IN_PROGRESS = 3;
+
+const PLAYER_WON_MSG_STRING = "YOU WON!!"
+const PLAYER_LOST_MSG_STRING = "YOU LOST!!"
+
 /**
  *   F U N C T I O N S
  */
 
 
 
+function get_game_status() {
 
+    if (g_current_score == g_targetNumber) {
+        return GAME_OVER_PLAYER_WON;
+    } else if (g_current_score > g_targetNumber) {
+        return GAME_OVER_PLAYER_LOST;
+    } else {
+        return GAME_IN_PROGRESS;
+    }
+}
 
+function restart_game() {
+    
+    if (g_game_status != GAME_IN_PROGRESS) {
+
+        //Restart the Game
+        module_init();
+    }
+}
 
 /**
  * Module Init Function
@@ -43,61 +82,128 @@ const CRYSTAL_POINTS_MAX = 12;
  */
 function module_init() {
 
-  console.log("at module INit")
+    g_game_status = GAME_IN_PROGRESS;
 
-  // Generate the (random) number that the user need to get to
-  g_targetNumber = Math.floor((Math.random() * (TARGET_NUMBER_MAX - TARGET_NUMBER_MIN + 1)) + TARGET_NUMBER_MIN);
+    // Generate the (random) number that the user need to get to
+    g_targetNumber = Math.floor((Math.random() * (TARGET_NUMBER_MAX - TARGET_NUMBER_MIN + 1)) + TARGET_NUMBER_MIN);
 
-  //Generate the (random) number for the four crystals
-  g_crystal1Points = Math.floor((Math.random() * (CRYSTAL_POINTS_MAX - CRYSTAL_POINTS_MIN + 1)) + CRYSTAL_POINTS_MIN);
-  g_crystal2Points = Math.floor((Math.random() * (CRYSTAL_POINTS_MAX - CRYSTAL_POINTS_MIN + 1)) + CRYSTAL_POINTS_MIN);
-  g_crystal3Points = Math.floor((Math.random() * (CRYSTAL_POINTS_MAX - CRYSTAL_POINTS_MIN + 1)) + CRYSTAL_POINTS_MIN);
-  g_crystal4Points = Math.floor((Math.random() * (CRYSTAL_POINTS_MAX - CRYSTAL_POINTS_MIN + 1)) + CRYSTAL_POINTS_MIN);
+    // Display the random number on the page
+    $("#target-score-tag").text(g_targetNumber);
 
-  g_current_score = 0;
+    //Generate the (random) number for the four crystals
+    g_crystal1Points = Math.floor((Math.random() * (CRYSTAL_POINTS_MAX - CRYSTAL_POINTS_MIN + 1)) + CRYSTAL_POINTS_MIN);
+    g_crystal2Points = Math.floor((Math.random() * (CRYSTAL_POINTS_MAX - CRYSTAL_POINTS_MIN + 1)) + CRYSTAL_POINTS_MIN);
+    g_crystal3Points = Math.floor((Math.random() * (CRYSTAL_POINTS_MAX - CRYSTAL_POINTS_MIN + 1)) + CRYSTAL_POINTS_MIN);
+    g_crystal4Points = Math.floor((Math.random() * (CRYSTAL_POINTS_MAX - CRYSTAL_POINTS_MIN + 1)) + CRYSTAL_POINTS_MIN);
+
+    $("#crystal-1-tag").html("<p class=\"text-warning\"> <small>Click To Show Points</small></p>");
+    $("#crystal-2-tag").html("<p class=\"text-warning\"> <small>Click To Show Points</small></p>");
+    $("#crystal-3-tag").html("<p class=\"text-warning\"> <small>Click To Show Points</small></p>");
+    $("#crystal-4-tag").html("<p class=\"text-warning\"> <small>Click To Show Points</small></p>");
+
+
+    // Init the current score and update on the web page
+    g_current_score = 0;
+    $("#current-score-tag").text(g_current_score);
+
+    // initially Crystals points are hidden
+    g_crystal1PointsHidden = true;
+    g_crystal2PointsHidden = true;
+    g_crystal3PointsHidden = true;
+    g_crystal4PointsHidden = true;
+
+
+    //update the Scoreboard - win and loss counts
+    $("#overall-wins-count").text(g_games_won);
+    $("#overall-loses-count").text(g_games_lost);
+
+
 }
 
 
 
 function m_crystal_click_handler(buttonClickEvent) {
 
-   //update the current score on the Web Page
-   $("#current-score-tag").empty(); 
 
-  // currentTarget in the event object refers to the Current DOM Element
-  // buttonClickEvent.currentTarget is EQUIVALENT to "this"
-  var crystalId = $(buttonClickEvent.currentTarget).attr("data-crystal-id");
-  var crystalCSSId = "#crystal-" + crystalId + "-tag";
+    //    //update the current score on the Web Page
+    //    $("#current-score-tag").text(g_targetNumber);
+    
 
-  switch (crystalId) {
+    // currentTarget in the event object refers to the Current DOM Element
+    // buttonClickEvent.currentTarget is EQUIVALENT to "this"
+    var crystalId = $(buttonClickEvent.currentTarget).attr("data-crystal-id");
+    var crystalCSSId = "#crystal-" + crystalId + "-tag";
 
-    case "1":
-      $(crystalCSSId).html("<h1 class=\"display-3\">" + g_crystal1Points + "</h1>");
-      g_current_score += g_crystal1Points;
-      break;
+    switch (crystalId) {
 
-    case "2":
-      $(crystalCSSId).html("<h1 class=\"display-3\">" + g_crystal2Points + "</h1>");
-      g_current_score += g_crystal2Points;
-      break;
+        case "1":
+            // if crystal 1 points are hidden then show the points, otherwise they are already being shown
+            if (g_crystal1PointsHidden === true) {
+                g_crystal1PointsHidden = false;
+                $(crystalCSSId).html("<h1 class=\"display-3\">" + g_crystal1Points + "</h1>");
+            }
+            g_current_score += g_crystal1Points;
+            break;
 
-    case "3":
-      $(crystalCSSId).html("<h1 class=\"display-3\">" + g_crystal3Points + "</h1>");
-      g_current_score += g_crystal3Points;
-      break;
+        case "2":
+            // if crystal 2 points are hidden then show the points, otherwise they are already being shown
+            if (g_crystal2PointsHidden === true) {
+                g_crystal2PointsHidden = false;
+                $(crystalCSSId).html("<h1 class=\"display-3\">" + g_crystal2Points + "</h1>");
+            }
+            g_current_score += g_crystal2Points;
+            break;
 
-    case "4":
-      $(crystalCSSId).html("<h1 class=\"display-3\">" + g_crystal4Points + "</h1>");
-      g_current_score += g_crystal4Points;
-      break;
+        case "3":
+            // if crystal 3 points are hidden then show the points, otherwise they are already being shown
+            if (g_crystal3PointsHidden === true) {
+                g_crystal3PointsHidden = false;
+                $(crystalCSSId).html("<h1 class=\"display-3\">" + g_crystal3Points + "</h1>");
+            }
+            g_current_score += g_crystal3Points;
+            break;
 
-    default:
-      console.log("Invalid Key Pressed")
-      break;
+        case "4":
+            // if crystal 4 points are hidden then show the points, otherwise they are already being shown
+            if (g_crystal4PointsHidden === true) {
+                g_crystal4PointsHidden = false;
+                $(crystalCSSId).html("<h1 class=\"display-3\">" + g_crystal4Points + "</h1>");
+            }
+            g_current_score += g_crystal4Points;
+            break;
 
-  }
-  
- 
+        default:
+            console.log("Invalid Key Pressed")
+            break;
+
+    }
+
+
+    // returns three possible values 
+    g_game_status = get_game_status();
+
+    // is Game Over ?
+    if (g_game_status != GAME_IN_PROGRESS) {
+
+        // Game is Over - update the Player win/loss count
+        if (g_game_status == GAME_OVER_PLAYER_WON) {
+            g_games_won++;
+            $("#current-score-tag").text(PLAYER_WON_MSG_STRING);
+        } else if (g_game_status == GAME_OVER_PLAYER_LOST) {
+            g_games_lost++;
+            $("#current-score-tag").text(PLAYER_LOST_MSG_STRING);
+        } else {
+            console.log("Invalid Game status !!")
+        }
+
+        setTimeout(restart_game, 1000);
+
+    } else {
+        // Game in Progress - update the current score on the Web Page
+        $("#current-score-tag").text(g_current_score);
+    }
+
+
 
 }
 
@@ -109,15 +215,11 @@ function m_crystal_click_handler(buttonClickEvent) {
  */
 $(document).ready(function () {
 
-  //Initialize module data
-  module_init();
+    //Initialize module data
+    module_init();
 
 
-  // Display the random number on the page
-  $("#target-score-tag").text(g_targetNumber);
-
-
-  $(".crystal").on("click", m_crystal_click_handler)
+    $(".crystal").on("click", m_crystal_click_handler)
 
 
 });
